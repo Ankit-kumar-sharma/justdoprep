937e030ea05cefec2a9150b18851f075b9c23c07
Ankit-kumar-sharma <ankitkumarsharmacse@gmail.com>
2021-09-28 15:43:11 +0530

    incomplete

---
+++ JavaScript/myscript.js
@@ -0,0 +1,9 @@
+function expand()
+{
+    // alert("Hello");
+    document.getElementById('Search').style="width:80%";
+}
+function shrink()
+{
+    document.getElementById('Search').style="width:0%";
+}
