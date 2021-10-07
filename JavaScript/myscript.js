e924cdef15e8bd31dda12d04a1faf1220d373941
Ankit-kumar-sharma <ankitkumarsharmacse@gmail.com>
2021-10-07 19:41:43 +0530

    update

--- JavaScript/myscript.js
+++ JavaScript/myscript.js
@@ -28,7 +28,28 @@ if(c==="fa fa-sun-o darkMode")
     document.getElementById('header').style="border-bottom:none;";
     document.getElementById('typingText').style="color:black; border-right:1px solid black";
     document.getElementById('Search').style="background-color:none; color:black; border-bottom:none;";
-  
+    document.getElementById('getStarted').style="color:black; border:1px solid black;";   
+    document.getElementById('getStarted').addEventListener('mouseover',function mouseOver()
+    {
+        document.getElementById('getStarted').style="color:white";
+    });
+   
+ 
+    document.getElementById('title').style="color:black;";
+    document.getElementById('courseinfo1').style="color:black; border:none;";
+    document.getElementById('courseinfo2').style="color:black; border:none;";
+    document.getElementById('courseinfo3').style="color:black; border:none;";
+    document.getElementById('courseinfo4').style="color:black; border:none;";
+    document.getElementById('jBtn1').style="color:black;";
+    document.getElementById('jBtn2').style="color:black;";
+    document.getElementById('jBtn3').style="color:black;";
+    document.getElementById('jBtn4').style="color:black;";
+    document.getElementById('footerLogo').src="./Images/graduation-hat.png";
+    document.getElementById('footer').style="color:black;";
+    document.getElementById('mentorinfo1').style="color:black";
+    document.getElementById('mentorinfo2').style="color:black";
+    document.getElementById('mentorinfo3').style="color:black";
+    document.getElementById('mentorinfo4').style="color:black";
 }
 else
 {
@@ -41,7 +62,31 @@ else
     {
         document.getElementById('searchIcon').style="color:white";
     });
+    document.getElementById('getStarted').addEventListener('mouseover',function mouseOver()
+    {
+        document.getElementById('getStarted').style="color:white; border:1px solid white; background: linear-gradient(to left,black 100%, white 0%); background-position:left;";
+    }); 
     
+    document.getElementById('getStarted').style="background:black; color:white; border:1px solid white;";
     document.getElementById('sun').className="fa fa-sun-o darkMode";
+    document.getElementById('courseinfo1').style="color:white; border:none";
+    document.getElementById('courseinfo2').style="color:white; border:none";
+    document.getElementById('courseinfo3').style="color:white; border:none";
+    document.getElementById('courseinfo4').style="color:white; border:none";
+    document.getElementById('jBtn1').style="color:white;";
+    document.getElementById('jBtn2').style="color:white;";
+    document.getElementById('jBtn3').style="color:white;";
+    document.getElementById('jBtn4').style="color:white;";
+    document.getElementById('mentorinfo1').style="color:white";
+    document.getElementById('mentorinfo2').style="color:white";
+    document.getElementById('mentorinfo3').style="color:white";
+    document.getElementById('mentorinfo4').style="color:white";
+    document.getElementById('title').style="color:white;";
+    document.getElementById('footer').style="color:white;";
+    document.getElementById('footerTitle').style="color:white;";
+    document.getElementsByClassName('fContent').style="color:white";
+    // document.querySelectorAll('fContainer4');
+    document.getElementById('footerLogo').src="./Images/logo.png";
+    document.getElementsByClassName('emailAddr').className="bColor";
 }
 }
